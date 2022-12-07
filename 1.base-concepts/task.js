2b76f9f00d015dbf51ae1826ac99622f7c6fc2c3
ITvladislavovna <107031721+ITvladislavovna@users.noreply.github.com>
2022-12-07 13:12:25 +0300

    Update task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,24 +1,45 @@
-"use strict"
+"use strict";
 function solveEquation(a, b, c) {
   let arr = [];
-  let d=b*b-4*a*c
-  console.log(d)
-    
-  if (d>0){
-    let x1=(-b + Math.sqrt(d) )/(2*a)
-    let x2=(-b - Math.sqrt(d) )/(2*a)
-    //console.log("KKK",x1,x2)
-  arr=[x1,x2]
+  let d = b  2 - 4 * a * c;
 
-  } else if (d==0){
-   let x1=-b/(2*a)
-   arr=[x1]
-     }
-     
-    return arr;
+  if (d > 0) {
+    let x1 = (-b + Math.sqrt(d)) / (2 * a);
+    let x2 = (-b - Math.sqrt(d)) / (2 * a);
+
+    arr = [x1, x2];
+  } else if (d == 0) {
+    let x1 = -b / (2 * a);
+    arr = [x1];
+  }
+
+  return arr;
 }
-//const result=solveEquation(1,5,4)
-//console.log("jjj",result)
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+function calculateTotalMortgage(percent, contribution, amount, date) {
+  if (isNaN(percent)) {
+    return false;
+  }
+  if (isNaN(contribution)) {
+    return false;
+  }
+  if (isNaN(amount)) {
+    return false;
+  }
+  if (isNaN(date)) {
+    return false;
+  }
+  let bodyCredit = 0;
+
+  let newPesent = percent / 100 / 12;
+  if (contribution) {
+    bodyCredit = amount - contribution;
+  } else {
+    bodyCredit = amount;
+  }
+
+  let monthlyPayment =
+    bodyCredit * (newPesent + newPesent / ((1 + newPesent)  date - 1));
+  let totalAmount = Number((monthlyPayment * date).toFixed(2));
+
+  return totalAmount;
 }
